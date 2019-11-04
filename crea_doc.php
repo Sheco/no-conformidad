ae441a6b41f5842fa1d89ebd50b88f541c9137fa
Sergio Duran <sergio@tiene.info>
2019-11-04 06:33:53 -0600

    Agregar chequeos de permisos al flujo de prueba

--- crea_doc.php
+++ crea_doc.php
@@ -1,4 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Gate;
+
 // cuantas veces rechazar
 $totalRechazos = 2;
 $personas = [
@@ -14,11 +17,13 @@ DB::table('propuestas')->truncate();
 DB::table('documentos')->truncate();
 
 $tipo = App\Tipo::find(1);
+$departamento = App\Departamento::find(1);
 
 /* paso 1, nuevo documento */
 echo "Creando nuevo documento...\n";
 $doc = new App\Documento;
-$doc->crear($barco, $tipo, 'huecote', 'hay un hueco');
+Gate::forUser($barco)->authorize('crear', App\Documento::class);
+$doc->crear($barco, $tipo, $departamento, 'huecote', 'hay un hueco');
 //$doc = App\Documento::find(5);
 espera();
 
@@ -26,6 +31,7 @@ $rechazos = 0;
 do {
     /* paso 2, se asigna responsable */ 
     echo "Asignando responsable...\n";
+    Gate::forUser($ism)->authorize('asignarResponsable', $doc);
     $doc->asignarResponsable($ism, $responsable);
     $doc->save();
     espera();
@@ -33,6 +39,7 @@ do {
     /* paso 3, el responsable agrega una propuesta */
     echo "Agregando propuesta...\n";
     $texto = ($rechazos<$totalRechazos? 'no hacer nada': 'tapar el hueco');
+    Gate::forUser($responsable)->authorize('agregarPropuesta', $doc);
     $propuesta = $doc->agregarPropuesta($responsable, $texto);
     $doc->save();
     espera();
@@ -40,6 +47,7 @@ do {
     /* paso 4, rechazar y reasignar */
     if($rechazos<$totalRechazos) {
         echo "Rechazando propuesta...\n";
+        Gate::forUser($ism)->authorize('rechazarPropuesta', $doc);
         $doc->rechazarPropuesta($ism, $propuesta, 'mala idea');
         $propuesta->save();
         $doc->save();
@@ -50,6 +58,7 @@ do {
 
 /* paso 5, aceptar la propuesta */
 echo "Aceptando propuesta...\n";
+Gate::forUser($ism)->authorize('aceptarPropuesta', $doc);
 $doc->aceptarPropuesta($ism, $propuesta, 'perfecto');
 $propuesta->save();
 $doc->save();
@@ -57,18 +66,21 @@ espera();
 
 /* paso 6, marcar como completado */
 echo "Marcando el documento como corregido...\n";
+Gate::forUser($responsable)->authorize('corregir', $doc);
 $doc->corregir($responsable);
 $doc->save();
 espera();
 
 /* paso 7, marcar como verificado */
 echo "Marcando el documento como verificado...\n";
+Gate::forUser($barco)->authorize('verificar', $doc);
 $doc->verificar($barco);
 $doc->save();
 espera();
 
 /* paso 9, cerrar el documento */
 echo "Marcando el documento como cerrado...\n";
+Gate::forUser($barco)->authorize('cerrar', $doc);
 $doc->cerrar($barco);
 $doc->save();
 espera();
