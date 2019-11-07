b89edfe2e8689b6ca0bffdfc3fd3245b1268d6b3
Sergio Duran <sergio@tiene.info>
2019-11-06 18:24:46 -0600

    Separar bloques de la forma de captura de usuario

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -7,5 +7,23 @@
         'title'=>'Nuevo usuario',
         'method'=>'post',
         ])
+  <div class="row" style="margin-top: 1em">
+      <div class="col-md-6">
+          <div class="card">
+              <div class="card-header">Departamentos suscritos</div>
+              <div class="card-body">
+                  Para asignar departamentos, primero guarde la forma de arriba.
+              </div>
+          </div>
+      </div>
+
+      <div class="col-md-6">
+          <div class="card">
+              <div class="card-header">Roles</div>
+              <div class="card-body">
+                  Para asignar roles, primero guarde la forma de arriba.
+              </div>
+          </div>
+  </div>
 </div>
 @endsection
