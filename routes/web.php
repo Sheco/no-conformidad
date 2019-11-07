4f64c2c51e6c9180cdd16dc476debcf3bd12e37e
Sergio Duran <sergio@tiene.info>
2019-11-07 12:24:32 -0600

    Limpieza de rutas

--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,33 @@ Route::middleware(['auth'])->group(function() {
     Route::redirect('/', '/docs');
     Route::redirect('/home', '/docs');
     
-    Route::get('/docs', 'DocumentosController@index');
-    Route::get('/docs/status/{status}', 'DocumentosController@index');
-    Route::get('/docs/{documento}', 'DocumentosController@ver');
-    Route::post('/docs/crear', 'DocumentosController@guardar');
-    Route::get('/docs/crear', 'DocumentosController@crear');
-    Route::post('/docs/{documento}/asignarResponsable', 'DocumentosController@asignarResponsable');
-    Route::post('/docs/{documento}/agregarPropuesta', 'DocumentosController@agregarPropuesta');
-    Route::post('/docs/{propuesta}/rechazarPropuesta', 'DocumentosController@rechazarPropuesta');
-    Route::post('/docs/{propuesta}/aceptarPropuesta', 'DocumentosController@aceptarPropuesta');
-    Route::post('/docs/{documento}/corregir', 'DocumentosController@corregir');
-    Route::post('/docs/{documento}/verificar', 'DocumentosController@verificar');
-    Route::post('/docs/{documento}/cerrar', 'DocumentosController@cerrar');
-    Route::get('/docs/{documento}/logs', 'DocumentosController@logs');
+    Route::get ('docs', 'DocumentosController@index');
+    Route::get ('docs/status/{status}', 'DocumentosController@index');
+    Route::get ('docs/{documento}', 'DocumentosController@ver');
+    Route::post('docs/crear', 'DocumentosController@guardar');
+    Route::get ('docs/crear', 'DocumentosController@crear');
+    Route::post('docs/{documento}/asignarResponsable', 
+        'DocumentosController@asignarResponsable');
+    Route::post('docs/{documento}/agregarPropuesta', 
+        'DocumentosController@agregarPropuesta');
+    Route::post('docs/{propuesta}/rechazarPropuesta', 
+        'DocumentosController@rechazarPropuesta');
+    Route::post('docs/{propuesta}/aceptarPropuesta', 
+        'DocumentosController@aceptarPropuesta');
+    Route::post('docs/{documento}/corregir', 'DocumentosController@corregir');
+    Route::post('docs/{documento}/verificar', 'DocumentosController@verificar');
+    Route::post('docs/{documento}/cerrar', 'DocumentosController@cerrar');
+    Route::get ('docs/{documento}/logs', 'DocumentosController@logs');
 
     Route::middleware(['role:admin'])->group(function() {
-        Route::resource('/admin/users', 'Admin\UsersController');
-        Route::post('/admin/users/{user}/delRole/{role}',
+        Route::resource('admin/users', 'Admin\UsersController');
+        Route::post('admin/users/{user}/delRole/{role}',
             'Admin\UsersController@delRole');
-        Route::post('/admin/users/{user}/addRole',
+        Route::post('admin/users/{user}/addRole',
             'Admin\UsersController@addRole');
-        Route::post('/admin/users/{user}/delDepartamento/{departamento}',
+        Route::post('admin/users/{user}/delDepartamento/{departamento}',
             'Admin\UsersController@delDepartamento');
-        Route::post('/admin/users/{user}/addDepartamento',
+        Route::post('admin/users/{user}/addDepartamento',
             'Admin\UsersController@addDepartamento');
 
         Route::resource('admin/departamentos', 'Admin\DepartamentosController');
